3603cbe24947d1e04335fdf1aaa6ac8cd39fde86
Luan Barbalho Kalume <luankalume@MacBook-Pro-de-Luan.local>
2015-03-18 11:41:09 -0300

    adicionada a função getAllFreeTime

--- MPCRevisited/service/ScheduleService.swift
+++ MPCRevisited/service/ScheduleService.swift
@@ -159,4 +159,93 @@ class ScheduleService {
         time.busy = false
         return time
     }
+    
+    /* Recebe um vetor com os horarios livres e opcionais dos usuarios
+        retornando outro array com dias e horas livres para exibição
+    */
+    func getAllFreeTime( horas:[Time] ) -> [resposta]
+    {
+        /* Cria o vetor com os resultados */
+        var resultados = [resposta]()
+        
+        
+        for( var x:Int = 0 ; x < horas.count ; x++ )
+        {
+            /* Se achar um horario livre */
+            if( horas[x].busy == false && horas[x].optional == false)
+            {
+                /* Cria uma nova entrada para ser introduzida no array */
+                var novaResp = resposta()
+                
+                /* Adiciona o dia na resposta */
+                novaResp.dia = horas[x].day
+                
+                /* Adiciona o horário inicial na resposta */
+                novaResp.hora = "Horário Inicial: \(horas[x].hour):00 "
+                
+                /* Olha os próximos elementos em busca de continuidade de horas livres até o final do dia
+                    ou até achar um horario não livre
+                */
+                for( var y:Int = x + 1 ; horas[y].hour != 7 ; y++ )
+                {
+                    /* Achando um horário não livre ou o "fim" do dia */
+                    if( horas[y].busy || horas[y].optional || horas[y].hour == 21)
+                    {
+                        /* Adiciona esse horário como final */
+                        novaResp.hora += "Horário Final: \(horas[y].hour):00"
+                        /* muda o valor do x para y e sai desse loop interno */
+                        x = y
+                        break;
+                    }
+                }
+                
+                /* Adiciona essa resposta nos resultados */
+                resultados.append(novaResp)
+                
+            }
+            else if( horas[x].optional == true )
+            {
+                /* Achando um opcional faz a mesma coisa, mas como opcional */
+                /* Cria uma nova entrada para ser introduzida no array */
+                var novaOpcional = resposta()
+                
+                /* Adiciona o dia na resposta com tag de opcional */
+                novaOpcional.dia = horas[x].day + "(Opcional)"
+                
+                /* Adiciona o horário inicial na resposta */
+                novaOpcional.hora = "Horário Inicial: \(horas[x].hour):00 "
+                
+                /* Olha os próximos elementos em busca de continuidade de horas opcionais até o final do dia
+                    ou até achar um horario não opcional
+                */
+                for( var y:Int = x + 1 ; horas[y].hour != 7 ; y++ )
+                {
+                    /* Achando um horário não opcional ou o "fim" do dia */
+                    if( horas[y].busy || horas[y].optional || horas[y].hour == 21)
+                    {
+                        /* Adiciona esse horário como final */
+                        novaOpcional.hora += "Horário Final: \(horas[y].hour):00"
+                        /* muda o valor do x para y e sai desse loop interno */
+                        x = y
+                        break;
+                    }
+                }
+                
+                /* Adiciona essa resposta nos resultados */
+                resultados.append(novaOpcional)
+            }
+            
+        }
+    
+        /* Retorna o vetor de resultados */
+        return resultados
+    }
+    
+    
+}
+
+class resposta
+{
+    var dia: String = ""
+    var hora: String = ""
 }
